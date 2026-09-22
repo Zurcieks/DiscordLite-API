d3cc842a0a17d4b4520767efab019f5f6ea29a92
Jazdan <jazdanrajabi@gmail.com>
2026-09-22 22:10:23 +0200

    added user avatar upload

---
+++ src/DiscordLite.Infrastructure/Persistence/Migrations/20260922173850_RenameUserAvatarUrlToAvatarKey.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordLite.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserAvatarUrlToAvatarKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvatarUrl",
+                table: "Users",
+                newName: "AvatarKey");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "Conversations",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Conversations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Conversations");
+
+            migrationBuilder.RenameColumn(
+                name: "AvatarKey",
+                table: "Users",
+                newName: "AvatarUrl");
+        }
+    }
+}
