8d8725278b3b312eeb24ea68ce7c3b1e7d717c18
Jazdan <jazdanrajabi@gmail.com>
2026-09-22 22:20:44 +0200

    added user avatar upload

--- src/DiscordLite.Application/Auth/Register/RegisterUserCommand.cs
+++ src/DiscordLite.Application/Auth/Register/RegisterUserCommand.cs
@@ -3,4 +3,4 @@ using MediatR;
 namespace DiscordLite.Application.Auth.Register;
 
 public sealed record RegisterUserCommand(string Username, string Password) : IRequest<RegisterUserResponse>;
-public sealed record RegisterUserResponse(Guid UserId, string Username, string? AvatarUrl, string AccessToken);
+public sealed record RegisterUserResponse(Guid UserId, string Username, string AccessToken);
