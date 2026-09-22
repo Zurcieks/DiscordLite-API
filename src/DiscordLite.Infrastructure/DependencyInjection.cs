93a5e0fa4e0150047196b696d751b6d45a38900b
Jazdan <jazdanrajabi@gmail.com>
2026-09-22 22:15:13 +0200

    added user avatar upload

--- src/DiscordLite.Infrastructure/DependencyInjection.cs
+++ src/DiscordLite.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,15 @@
+using System.Runtime.InteropServices.ComTypes;
 using DiscordLite.Application.Abstractions;
 using DiscordLite.Infrastructure.BackgroundServices;
 using DiscordLite.Infrastructure.Persistence;
 using DiscordLite.Infrastructure.Persistence.Repositories;
 using DiscordLite.Infrastructure.Security;
+using DiscordLite.Infrastructure.Storage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Minio;
 
 namespace DiscordLite.Infrastructure;
 
@@ -20,7 +24,27 @@ public static class DependencyInjection
         {
             options.UseNpgsql(connectionString);
         });
+
+        services.Configure<MinioOptions>(
+            configuration.GetSection("Minio")); 
+        // laczy json z MiniopOptions, zeby inne uslugi mogly pobrac ustawienia przez ioptions
+
+        services.AddSingleton<IMinioClient>(provider =>
+        {
+            var options = provider
+                .GetRequiredService<IOptions<MinioOptions>>()
+                .Value;
+
+            return new MinioClient()
+                .WithEndpoint(options.Endpoint)
+                .WithCredentials(options.AccessKey, options.SecretKey)
+                .WithSSL(options.UseSsl)
+                .Build();
+        });
         
+        services.AddScoped<IAvatarContentValidator, AvatarContentValidator>();
+        services.AddScoped<IAvatarStorage, AvatarStorage>();
+        services.AddHostedService<MinioBucketInitializer>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IPasswordService, PasswordService>();
         services.Configure<JwtOptions>(configuration.GetSection("Jwt"));
@@ -29,6 +53,7 @@ public static class DependencyInjection
         services.AddHostedService<RefreshTokenCleanupService>();
         services.AddScoped<IFriendshipRepository, FriendshipRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IConversationRepository, ConversationRepository>();
         return services;
     }
 }
